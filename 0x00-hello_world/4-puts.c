0c5d142988c559c5deb4e1e4a8d4afa24d66e2fb
jomex2010 <jomex2010@gmail.com>
2022-10-13 17:32:19 -0700

    task for seen uneasy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
- * main-Serves as the entry point for the program
- *
- * Return:By default returns zero if no errors otherwise returns non-zero value.*/
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
-	puts("\"Pramming is like building a multilingual puzzle"); return (0);
+	puts("\"Programming is like building a multilingual puzzle") ;
+	return (0) ;
 }
