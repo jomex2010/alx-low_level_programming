81caca9974ba9c66d2fccdf0a8950e12e36492f4
jomex2010 <jomex2010@gmail.com>
2022-10-18 14:24:16 -0700

    very hard to come by now

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,8 +13,7 @@ int main(void)
 		putchar(n);
 		if (n != 57)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(",", n);
 		}
 	putchar('\n');
 	return (0);
---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * is written by bwave
+ * Return:0
+ */
+
+int main(void)
+{
+	char ch = '_'
+		putchar('_');
+		putchar('p');
+		putchar('u');
+		putchar('t');
+		putchar('c');
+		putchar('h');
+		putchar('a');
+		putchar('r');
+		putchar('\n');
+		return (0);
+}
