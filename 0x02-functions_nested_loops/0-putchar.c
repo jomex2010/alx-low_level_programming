a1ee101646b175ff4604311bc5eb88b2c8f4af4b
jomex2010 <jomex2010@gmail.com>
2022-10-19 00:48:03 -0700

    wow o wow

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
 /**
- * main - print _putchar
+ * main - prints  _putchar
  * is written by bwave
- * Return:0
+ * Return: Always 0 (Success)
  */
-
-int main(void)
+int main(void);
 {
-	char ch = '_'
-		putchar('_');
-		putchar('p');
-		putchar('u');
-		putchar('t');
-		putchar('c');
-		putchar('h');
-		putchar('a');
-		putchar('r');
-		putchar('\n');
-		return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
